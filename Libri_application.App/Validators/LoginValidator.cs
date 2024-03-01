447917cf16c5689fc4b02abaf5c6000f6e663575
francescopiz <francy.pizzuto@gmail.com>
2024-03-01 22:51:21 +0100

    creazione validator per RecensioneDelete e Login

---
+++ Libri_application.App/Validators/LoginValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Libri_application.App.Models.Requests;
+
+namespace Libri_application.App.Validators
+{
+    public class LoginValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.login).
+                NotNull().
+                WithMessage("Il campo non può essere nullo")
+                .NotEmpty()
+                .WithMessage("Il campo login non può essere vuoto");
+                
+            RuleFor(x => x.password).
+                NotNull().
+                WithMessage("Il campo non può essere nullo")
+                .NotEmpty()
+                .WithMessage("Il campo password non può essere vuoto");
+        }
+    }
+}
---
+++ Libri_application.App/Validators/RecensioneDeleteValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Libri_application.App.Models.Requests;
+
+namespace Libri_application.App.Validators
+{
+    public class RecensioneDeleteValidator : AbstractValidator<RecensioneDelete>
+    {
+        public RecensioneDeleteValidator()
+        {
+            RuleFor(x => x.idLibro)
+                .NotNull()
+                .WithMessage("Il campo non può essere nullo")
+                .NotEmpty()
+                .WithMessage("Il campo login non può essere vuoto");
+        }
+    }
+}
